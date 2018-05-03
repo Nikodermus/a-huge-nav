cfd97f6514cccc685b4a95407776964f006c038d
Nicolás M. Pardo <nicolasm@nikodermus.media>
2018-05-02 22:18:38 -0500

    feat(Mobile Nav): Display sub-items
    On click on the large item, the sub items list will appear
    
    Breaking change: Scroll for mobile-nav__content

--- public/javascript/main.js
+++ public/javascript/main.js
@@ -13,8 +13,31 @@ function triggerNav() {
   }
 }
 
+function triggerSub(e) {
+  const li = e.target.closest('.mobile-pri__item--inner');
+  const sub_menu = li.querySelector('.mobile-sub');
+
+  const sub_open = sub_menu.classList.contains('mobile-sub--show');
+
+  if (sub_open) {
+    li.style.marginBottom = `0`;
+    li.classList.remove('mobile-pri__item--open');
+    sub_menu.classList.remove('mobile-sub--show');
+  } else {
+    li.style.marginBottom = `${sub_menu.offsetHeight}px`;
+    li.classList.add('mobile-pri__item--open');
+    sub_menu.classList.add('mobile-sub--show');
+  }
+
+}
+
 
 // Wait for elements from the DOM
 document.addEventListener('DOMContentLoaded', () => {
   document.querySelector('.toogle').addEventListener('click', triggerNav)
+
+  const mob_sub_items = [...document.querySelectorAll('.mobile-pri__item--inner')];
+  mob_sub_items.map(elem => {
+    elem.addEventListener('click', triggerSub)
+  });
 });
