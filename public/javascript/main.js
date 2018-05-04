bac75c262834bbf4c80f1e2aec92974e400576ff
Nicolás M. Pardo <nicolasm@nikodermus.media>
2018-05-03 21:57:37 -0500

    fix(Navigation): General Adjustment
    * Added JS hover effect on desktop    * Added event listeners for only certain screen sizes    *
    Minor style adjustments

--- public/javascript/main.js
+++ public/javascript/main.js
@@ -1,15 +1,25 @@
+// Global elements
+let toggle,
+  sub_items,
+  site_container
+
 // Should the nav be opened or closed?
 function triggerNav() {
   const nav_mobile = document.querySelector('.main-nav');
-  const site_container = document.querySelector('.site-container');
   const nav_open = nav_mobile.classList.contains('main-nav--open');
 
   if (nav_open) {
+    //   Close menu
     nav_mobile.classList.remove('main-nav--open');
-    site_container.classList.remove('site-container--open');
+    site_container.classList.remove('site-container--open')
+    document.body.classList.remove('scroll-hidden');
+    document.documentElement.classList.remove('scroll-hidden');
   } else {
+    //   Open menu
     nav_mobile.classList.add('main-nav--open');
     site_container.classList.add('site-container--open');
+    document.body.classList.add('scroll-hidden');
+    document.documentElement.classList.add('scroll-hidden');
   }
 }
 
@@ -22,24 +32,25 @@ function triggerSub(e) {
   const sub_open = sub_menu.classList.contains('sub-nav--show');
 
   if (sub_open) {
-    //  Close the mobile nav
-    li.style.marginBottom = `0`;
+    //  Close the mobile sub nav
+    li.style.marginBottom = '0';
     li.classList.remove('pri-nav__item--open');
     sub_menu.classList.remove('sub-nav--show');
     scrollToTop(menu_container, 200)
   } else {
-    //  Open the mobile nav
+    //  Open the mobile sub nav
     li.style.marginBottom = `${sub_menu.offsetHeight}px`;
     li.classList.add('pri-nav__item--open');
     sub_menu.classList.add('sub-nav--show');
   }
 
+  //  Should the content be scrollable?
   if (
     menu_pri.offsetHeight > menu_container.offsetHeight
   ) {
     menu_container.style.overflowY = 'hidden';
   } else {
-    menu_container.style.overflowY = 'scroll';
+    menu_container.style.overflowY = 'auto';
   }
 }
 
@@ -54,15 +65,57 @@ function scrollToTop(elem, duration = 500) {
   }, time_interval);
 }
 
+function mobileEventListeners() {
+  toggle.addEventListener('click', triggerNav)
+  sub_items.map(elem => {
+    elem.addEventListener('click', triggerSub)
+
+    // Remove hover effect by js in mobile
+    elem.removeEventListener('mouseenter', () => {
+      site_container.classList.add('site-container--open');
+    });
+
+    elem.removeEventListener('mouseleave', () => {
+      site_container.classList.remove('site-container--open');
+    });
+  });
+}
+
+function desktopEventListeners() {
+  toggle.removeEventListener('click', triggerNav)
+  sub_items.map(elem => {
+    elem.removeEventListener('click', triggerSub)
+
+    // Add hover effect by js in desktop
+    elem.addEventListener('mouseenter', () => {
+      site_container.classList.add('site-container--open');
+    });
+
+    elem.addEventListener('mouseleave', () => {
+      site_container.classList.remove('site-container--open');
+    });
+  });
+
+}
+
+function isMobile() {
+  return window.innerWidth < 960
+}
+
+window.addEventListener('resize', () => {
+  isMobile() ? mobileEventListeners() : desktopEventListeners();
+});
 
 // Wait for elements from the DOM
 document.addEventListener('DOMContentLoaded', () => {
-  document.querySelector('.toogle').addEventListener('click', triggerNav)
 
-  const mob_sub_items = [...document.querySelectorAll('.pri-nav__item--inner')];
-  mob_sub_items.map(elem => {
-    elem.addEventListener('click', triggerSub)
-  });
+  // Load dom elements in variables
+  sub_items = [...document.querySelectorAll('.pri-nav__item--inner')];
+  toggle = document.querySelector('.toggle');
+  site_container = document.querySelector('.site-container');
+
+  // Wether add or remove event listeners
+  isMobile() ? mobileEventListeners() : desktopEventListeners();
 
   // Set menu on top
   scrollToTop(document.querySelector('.main-nav__content'), 200);
